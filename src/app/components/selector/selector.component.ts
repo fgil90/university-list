9b61a75b661a280340b868bef20e8062fd0e9688
FGil <fgil90@gmail.com>
2022-01-15 19:59:17 +0000

    add dynamic country list to option

--- src/app/components/selector/selector.component.ts
+++ src/app/components/selector/selector.component.ts
@@ -1,22 +1,31 @@
-import { Component, OnInit, Input, } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-selector',
   templateUrl: './selector.component.html',
-  styleUrls: ['./selector.component.css']
+  styleUrls: ['./selector.component.css'],
 })
 export class SelectorComponent implements OnInit {
-  
-  @Input() type: string = "";
+  @Input() type: string = '';
+  @Input() countries = new Set();
+  dataURL: string = 'http://universities.hipolabs.com/search';
+  data: Object = {};
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    if (this.type == 'country') this.fetchCountryData();
+  }
+
+  async fetchCountryData() {
+    const testData = await fetch(this.dataURL);
+    const response = await testData.json();
+    this.data = response;
+
+    response.forEach((element: any) => {
+      this.countries.add(element[this.type]);
+    });
+    console.log(this.countries);
     
   }
-
-  fetchData(){
-    console.log(this.type);
-  }
-
 }
